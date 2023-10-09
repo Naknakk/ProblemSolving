77286b9e6cd6b2cd421ec6fc2c3b5dcf263a10d6
YunHak Lee <87518742+Naknakk@users.noreply.github.com>
2023-10-09 15:12:54 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 308 ms, Memory: 88604 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.swift
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.swift
@@ -1,56 +1,61 @@
-let rc = readLine()!.split{$0 == " "}.map{Int(String($0))!}
-let r = rc[0], c = rc[1]
-
-var map: [[Int]] = Array(repeating: [], count: r)
-for i in 0..<r {
-    map[i] = Array(readLine()!).map{Int(String($0))!}
+let NM = readLine()!.split{$0 == " "}.map{Int(String($0))!}
+let N = NM[0], M = NM[1]
+var map: [[Int]] = []
+for _ in 0..<N {
+    map.append(Array(readLine()!).map{Int(String($0))!})
 }
-var visited: [[[Int]]] = Array(repeating: Array(repeating: Array(repeating: 0,
-                                                                  count: 2),
-                                                 count: c), 
-                                count: r)
-visited[0][0][0] = 1
+var que: [(Int, Int)] = [(0, 0)]
+var broken: [Bool] = [false]
+let dr = [-1, 1, 0, 0], dc = [0, 0, -1, 1]
+var visited = 2
+var count = 1
+var exit = (N==1 && M==1)
+var result = exit ? 1 : -1
+map[0][0] = visited
 
-var que: [(Int, Int, Int)] = [(0, 0, 0)]
-var pointer = 0
-let dr = [1, -1, 0, 0], dc = [0, 0, 1, -1]
-var result = -1
-
-bfs: while pointer < que.count {
-    let cur = que[pointer]
+func bfs() {
+    var nextQue: [(Int, Int)] = []
+    var nextBroken: [Bool] = []
     
-    for i in 0..<4 {
-        let nr = cur.0+dr[i], nc = cur.1+dc[i]
+    bfs: for i in 0..<que.count {
+        let cur = que[i]
         
-        if nr<0 || nr>r-1 || nc<0 || nc>c-1 {
-            continue
-        }
-        
-        if cur.2 == 0 {
-            if map[nr][nc] == 1 && visited[nr][nc][1] == 0 {
-                visited[nr][nc][1] = visited[cur.0][cur.1][cur.2] + 1
-                que.append((nr, nc, 1))
-                
+        for j in 0..<4 {
+            let nr = cur.0+dr[j]
+            let nc = cur.1+dc[j]
+            
+            if nr<0 || nr>N-1 || nc<0 || nc>M-1 {
+                continue
             }
-            if map[nr][nc] == 0 && visited[nr][nc][0] == 0 {
-                visited[nr][nc][0] = visited[cur.0][cur.1][cur.2] + 1
-                que.append((nr, nc, 0))
-            }
-        } 
-        
-        if cur.2 == 1 {
-            if map[nr][nc] == 0 && visited[nr][nc][1] == 0 {
-                visited[nr][nc][1] = visited[cur.0][cur.1][cur.2] + 1
-                que.append((nr, nc, 1))
-            }
+            
+            if (map[nr][nc] == 0 || map[nr][nc] == 3) && !broken[i] {
+                map[nr][nc] = 4
+                nextQue.append((nr, nc))
+                nextBroken.append(broken[i])
+            } else if map[nr][nc] == 1 && !broken[i] {
+                map[nr][nc] = 2
+                nextQue.append((nr, nc))
+                nextBroken.append(true)
+            } else if map[nr][nc] == 0 && broken[i] {
+                map[nr][nc] = 3
+                nextQue.append((nr, nc))
+                nextBroken.append(broken[i])
+            } 
         }
     }
+    que = nextQue
+    broken = nextBroken
+}
+
+while !exit {
+    count += 1
+    bfs()
     
-    if visited[r-1][c-1][0] != 0 || visited[r-1][c-1][1] != 0 {
-        result = max(visited[r-1][c-1][0], visited[r-1][c-1][1])
-        break bfs
+    if map[N-1][M-1] != 0 {
+        result = count
+        exit = true
+    } else if que.isEmpty {
+        exit = true
     }
-    
-    pointer += 1
 }
 print(result)
