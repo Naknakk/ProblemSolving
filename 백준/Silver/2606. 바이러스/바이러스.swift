4f5c714ba2c8b3f785f5d45cb445cf1027c47219
YunHak Lee <87518742+Naknakk@users.noreply.github.com>
2023-10-09 15:23:39 +0900

    [Silver III] Title: 바이러스, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.swift
+++ 백준/Silver/2606. 바이러스/바이러스.swift
@@ -1,32 +1,28 @@
 let computerNumber = Int(readLine()!)!
-var isInfection = Array(repeating: false, count: computerNumber)
-isInfection[0] = true
+var infections = Array(repeating: false, count: computerNumber)
+infections[0] = true
 
 let connectNumber = Int(readLine()!)!
-var connections: [Int : [Int]] = [:]
+var connections: [[Int]] = Array(repeating: [], count: computerNumber)
 for i in 0..<connectNumber {
     let connection = readLine()!.split{$0 == " "}.map{Int(String($0))!-1}
-    connections[connection[0], default: []].append(connection[1])
-    connections[connection[1], default: []].append(connection[0])
+    connections[connection[0]].append(connection[1])
+    connections[connection[1]].append(connection[0])
 }
 
 var queue: [Int] = [0]
-var queueCounter = 1
 var pointer = 0
 
-while pointer != queueCounter {
+while pointer < queue.count {
     let cur = queue[pointer]
-    
-    for next in connections[cur] ?? [] {
-        if isInfection[next] {
-            continue
-        } else {
-            isInfection[next] = true
+   
+    for next in connections[cur] {
+        if !infections[next] {
+            infections[next] = true
             queue.append(next)
-            queueCounter += 1
         }
     }
     pointer += 1
 }
 
-print(queueCounter-1)
+print(queue.count-1)
