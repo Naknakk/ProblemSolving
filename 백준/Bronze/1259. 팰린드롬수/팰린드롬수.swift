56c67826e1982f57ebdbdcbe961cb56af0889def
YunHak Lee <87518742+Naknakk@users.noreply.github.com>
2023-10-09 14:55:59 +0900

    [Bronze I] Title: 팰린드롬수, Time: 8 ms, Memory: 69096 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.swift
@@ -0,0 +1,17 @@
+while let input = readLine() {
+    if input == "0" {
+        break
+    }
+    let test = Array(input)
+    let count = test.count
+    let reverse = Array(String(test.reversed()))
+    var flag = true
+    
+    for index in 0..<count {
+        if test[index] != reverse[index] {
+            flag = false
+            break
+        }
+    }
+    print(flag ? "yes" : "no")
+}
