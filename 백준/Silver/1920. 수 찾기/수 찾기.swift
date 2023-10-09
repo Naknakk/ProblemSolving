804a61dcb0b86ca6247ad097ccb852e29beed38d
YunHak Lee <87518742+Naknakk@users.noreply.github.com>
2023-10-09 15:12:05 +0900

    [Silver IV] Title: 수 찾기, Time: 168 ms, Memory: 81396 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.swift
+++ 백준/Silver/1920. 수 찾기/수 찾기.swift
@@ -1,30 +1,9 @@
 let N = Int(readLine()!)!
-let numbers = readLine()!.split{$0 == " "}.map{Int(String($0))!}.sorted()
+let numbers = Set(readLine()!.split{$0 == " "}.map{Int(String($0))!})
 
 let M = Int(readLine()!)!
 let testCases = readLine()!.split{$0 == " "}.map{Int(String($0))!}
 
-func binarySearch(of testNumber: Int) -> Int {
-    var loIndex = -1
-    var hiIndex = N
-    
-    while loIndex + 1 < hiIndex {
-        let midIndex = (loIndex+hiIndex)/2
-        
-        if testNumber <= numbers[midIndex] {
-            hiIndex = midIndex
-        } else {
-            loIndex = midIndex
-        }
-    }
-    
-    if loIndex == N-1 {
-        return 0
-    } else {
-        return numbers[hiIndex] == testNumber ? 1 : 0
-    }
-}
-
 for testCase in testCases {
-    print(binarySearch(of: testCase))
+    print(numbers.contains(testCase) ? "1" : "0")
 }
