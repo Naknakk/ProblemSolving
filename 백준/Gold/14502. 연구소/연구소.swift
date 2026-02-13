5bcdc3c3764a9f837997e1c0eacb56ed3b139808
YunHak Lee <87518742+Naknakk@users.noreply.github.com>
2026-02-13 15:44:20 +0900

    [Gold IV] Title: 연구소, Time: 164 ms, Memory: 72012 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.swift
@@ -0,0 +1,79 @@
+let NM = readLine()!.split{$0 == " "}.map{Int(String($0))!}
+let N = NM[0]
+let M = NM[1]
+
+var map: [[Int]] = []
+for _ in 0..<N {
+    let row = readLine()!.split{$0 == " "}.map{Int(String($0))!}
+    map.append(row)
+}
+
+var everyEmtpySpaces: [(Int, Int)] = []
+for i in 0..<N {
+    for j in 0..<M {
+        if map[i][j] == 0 {
+            everyEmtpySpaces.append((i, j))
+        }
+    }
+}
+let limit = everyEmtpySpaces.count
+var wallCases: [[Int]] = []
+for i in 0..<limit-2 {
+    for j in (i+1)..<limit-1 {
+        for k in (j+1)..<limit {
+            wallCases.append([i, j, k])
+        }
+    }
+}
+
+var ans = 0
+let safe = 0
+let wall = 1
+let virus = 2
+let dr = [1, -1, 0, 0]
+let dc = [0, 0, 1, -1]
+
+for wallCase in wallCases {
+    var currentMap = map
+    let coors = wallCase.map{everyEmtpySpaces[$0]}
+    for coor in coors {
+        currentMap[coor.0][coor.1] = wall
+    }
+    var visitMap = Array(repeating: Array(repeating: false, count: M), count: N)
+    
+    for i in 0..<N {
+        for j in 0..<M {
+            if currentMap[i][j] != virus || visitMap[i][j] {
+                continue
+            }
+            
+            var queue: [(Int, Int)] = [(i, j)]
+            var pointer = 0
+            while pointer < queue.count {
+                let cur = queue[pointer]
+                
+                for k in 0..<4 {
+                    let nr = cur.0 + dr[k]
+                    let nc = cur.1 + dc[k]
+                    
+                    if nr<0 || nr>=N || nc<0 || nc>=M {
+                        continue
+                    }
+                    if visitMap[nr][nc] || currentMap[nr][nc] == wall {
+                        continue
+                    }
+                    
+                    visitMap[nr][nc] = true
+                    currentMap[nr][nc] = virus
+                    queue.append((nr, nc))
+                }
+                pointer += 1
+            }
+        }
+    }
+    
+    let result = currentMap.flatMap{$0}.filter{$0 == safe}.count
+    ans = max(ans, result)
+}
+
+print(ans)
