57f991835448c507267499f2b6b4e17a974914b2
YunHak Lee <87518742+Naknakk@users.noreply.github.com>
2026-03-02 14:54:17 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.swift
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.swift
@@ -3,7 +3,7 @@ var sequence: [Int] = readLine()!.split{$0 == " "}.map{Int(String($0))!}
 var dp: [Int] = Array(repeating: 0, count: N)
 dp[0] = sequence[0]
 
-for i in 0..<N {
+for i in 1..<N {
     var sum = 0
     for j in stride(from: i-1, through: 0, by: -1) {
         if sequence[j] < sequence[i] {
