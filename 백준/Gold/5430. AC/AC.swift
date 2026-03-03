a5b60948d3c0d6ee0bca2d57632f364987a1b3e9
YunHak Lee <87518742+Naknakk@users.noreply.github.com>
2026-03-03 15:01:26 +0900

    [Gold V] Title: AC, Time: 200 ms, Memory: 79700 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.swift
@@ -0,0 +1,61 @@
+// R: 배열에 있는 수의 순서 뒤집기
+// D: 배열의 첫 번째 수를 버리기, 비어있을 때 사용하면 에러.
+// 함수는 조합해서 한 번에 사용 가능. 
+// - RDD: 뒤집기, 첫 수 버리기, 첫 수 버리기
+// 배열 초기값과 수행할 함수 -> 최종 결과 배열로 출력, 에러 발생 시 error
+
+let T = Int(readLine()!)!
+
+for _ in 0..<T {
+    let sequence: [String] = Array(readLine()!).map{String($0)}
+    let N = Int(readLine()!)!
+    let array: [String] = readLine()!.split{$0 == "," || $0 == "[" || $0 == "]"}
+                            .map{String($0)}
+    
+    var st = 0
+    var en = N
+    var reversed = false
+    var isError = false
+    
+    Inner: for op in sequence {
+        if op == "R" {
+            if reversed {
+                let temp = st+1
+                st = en+1
+                en = temp
+            } else {
+                let temp = st-1
+                st = en-1
+                en = temp
+            }
+            reversed.toggle()
+        } else if op == "D" {
+            if st == en {
+                isError = true
+                break Inner
+            } else {
+                st = reversed ? st - 1 : st + 1
+            }
+        }
+    }
+    
+    if isError {
+        print("error")
+    } else {
+        var result: [String] = []
+        
+        if reversed {
+            for i in stride(from: st, to: en, by: -1) {
+                result.append(array[i])
+            }
+        } else {
+            for i in st..<en {
+                result.append(array[i])
+            }
+        }
+        let output = "[\(result.joined(separator: ","))]"
+        print(output)
+        
+    }
+    
+}
